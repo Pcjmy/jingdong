e97b26ecb8c0262b090f655b1b2562b6c6b7ece6
Pcjmy <1716045191@qq.com>
2022-10-03 22:41:42 +0800

    搜索框及banner区域设计

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,16 @@
         郑州大学新校区柳园3号楼2层
         <span class="iconfont position_notice">&#xe60b;</span>
       </div>
+      <div class="search">
+        <span class="iconfont">&#xe62d;</span>
+        <span class="search__text">山姆会员商店优惠商品</span>
+      </div>
+      <div class="banner">
+        <img
+          class="banner__img"
+          src="http://www.dell-lee.com/imgs/vue3/banner.jpg"
+        />
+      </div>
   </div>
   <div class="docker">
     <div class="docker__item docker__item--active">
@@ -55,6 +65,31 @@
     font-size: .2rem;
   }
   color: $content-fontcolor;
+}
+.search {
+  margin-bottom: .12rem;
+  line-height: .32rem;
+  background: #F5F5F5;
+  color: #B7B7B7;
+  border-radius: .16rem;
+  .iconfont {
+    display: inline-block;
+    padding: 0 .08rem 0 .16rem;
+    font-size: .16rem;
+  }
+  &__text {
+    display: inline-block;
+    font-size: .14rem;
+  }
+}
+
+.banner {
+  height: 0;
+  overflow: hidden;
+  padding-bottom: 25.4%;
+  &__img {
+    width: 100%;
+  }
 }
 .docker {
   display: flex;
