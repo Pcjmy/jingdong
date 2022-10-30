a23d664fa8945b3431c9da150bfeb4a93f1de327
Pcjmy <1716045191@qq.com>
2022-10-30 13:59:30 +0800

    订单页面布局

--- src/router/index.js
+++ src/router/index.js
@@ -4,10 +4,14 @@ const routes = [{
   path: '/',
   name: 'Home',
   component: () => import(/* webpackChunkName: "home" */ '../views/home/Home')
-}, {
+},{
   path: '/cartList',
   name: 'CartList',
   component: () => import(/* webpackChunkName: "cartList" */ '../views/cartList/CartList')
+},{
+  path: '/orderConfirmation/:id',
+  name: 'OrderConfirmation',
+  component: () => import(/* webpackChunkName: "orderConfirmation" */ '../views/orderConfirmation/OrderConfirmation')
 }, {
   path: '/shop/:id',
   name: 'Shop',
@@ -16,17 +20,17 @@ const routes = [{
   path: '/register',
   name: 'Register',
   component: () => import(/* webpackChunkName: "register" */ '../views/register/Register'),
-  beforeEnter (to, from, next) {
-    const { isLogin } = localStorage
-    isLogin ? next({ name: 'Home' }) : next()
+  beforeEnter(to, from, next) {
+    const { isLogin } = localStorage;
+    isLogin ? next({ name: 'Home'}):  next();
   }
 }, {
   path: '/login',
   name: 'Login',
   component: () => import(/* webpackChunkName: "login" */ '../views/login/Login'),
-  beforeEnter (to, from, next) {
-    const { isLogin } = localStorage
-    isLogin ? next({ name: 'Home' }) : next()
+  beforeEnter(to, from, next) {
+    const { isLogin } = localStorage;
+    isLogin ? next({ name: 'Home'}):  next();
   }
 }
 ]
@@ -36,11 +40,11 @@ const router = createRouter({
   routes
 })
 
-router.beforeEach((to, from, next) => {
-  const { isLogin } = localStorage
-  const { name } = to
-  const isLoginOrRegister = (name === 'Login' || name === 'Register');
-  (isLogin || isLoginOrRegister) ? next() : next({ name: 'Login' })
+router.beforeEach((to, from ,next) => {
+  const { isLogin } = localStorage;
+  const { name } = to;
+  const isLoginOrRegister = (name === "Login" || name === "Register");
+  (isLogin || isLoginOrRegister) ? next() : next({ name: 'Login'});
 })
 
 export default router
