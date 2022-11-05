47c11fa1959ca54e05c3b3f62edd34303eb99208
Pcjmy <1716045191@qq.com>
2022-11-06 00:44:01 +0800

    购物车

--- src/views/orderConfirmation/TopArea.vue
+++ src/views/orderConfirmation/TopArea.vue
@@ -11,7 +11,7 @@
         <div class="top__receiver__title">收货地址</div>
         <div class="top__receiver__address">北京理工大学国防科技园2号楼10层</div>
         <div class="top__receiver__info">
-          <span class="top__receiver__info__name">瑶妹（先生）</span>
+          <span class="top__receiver__info__name">倒流时间（先生）</span>
           <span class="top__receiver__info__name">18911024266</span>
         </div>
         <div class="iconfont top__receiver__icon">&#xe6f2;</div>
